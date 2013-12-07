2f07db5daaab2b930a686b2ded2cf5b2d42ef0fa
Dan Kerrigan <dankerrigan@basho.com>
2013-12-06 23:22:33 -0500

    Added multi-put

--- src/cre_wm_multi.erl
+++ src/cre_wm_multi.erl
@@ -4,7 +4,9 @@
          service_available/2,
          allowed_methods/2,
          to_json/2,
+         accept_json/2,
          content_types_provided/2,
+         content_types_accepted/2,
          resource_exists/2
         ]).
 
@@ -32,14 +34,28 @@ service_available(ReqData, Context=#ctx{}) ->
     }.
 
 allowed_methods(ReqData, Context) ->
-    {['GET'], ReqData, Context}.
+    {['GET', 'PUT'], ReqData, Context}.
 
 %% For simplicity, we're assuming all objects are json or plain text
 content_types_provided(ReqData, Context) ->
     {[{"application/json", to_json}], ReqData, Context}.
 
+content_types_accepted(ReqData, Context) ->
+    {[{"application/json", accept_json}], ReqData, Context}.
+
 to_json(ReqData, Context) ->
     {Context#ctx.objects, ReqData, Context}.
+
+accept_json(ReqData, Context) ->
+    Body = wrq:req_body(ReqData),
+    Json = mochijson2:decode(Body),
+    Results = multi_put(Context#ctx.bucket, Json, []),
+    JsonResults = mochijson2:encode(Results),
+    {
+      true,
+      wrq:set_resp_body(JsonResults, ReqData),
+      Context
+    }.
 
 resource_exists(ReqData, Context) ->
     C1 = ensure_objects(Context),
@@ -51,20 +67,24 @@ resource_exists(ReqData, Context) ->
 %%% Internal functions
 
 ensure_objects(Context) ->
-    KeyList = string:tokens(Context#ctx.keys, ","),
-    Results = build_results(Context#ctx.bucket, KeyList, []),
-    Context#ctx{objects = Results}.
+    case Context#ctx.keys of
+      undefined ->
+        Context;
+      _ ->
+        KeyList = string:tokens(Context#ctx.keys, ","),
+        Results = multi_get(Context#ctx.bucket, KeyList, []),
+        Context#ctx{objects = Results}
+      end.
 
-build_results(Bucket, [Key|Rest], Results) ->
+multi_get(Bucket, [Key|Rest], Results) ->
     Value = case get(client(), list_to_binary(Bucket), list_to_binary(Key)) of
-                {value, V} ->
-                    binary_to_list(V);
-                {error, Reason} ->
-                    Reason
+              {value, V} ->
+                binary_to_list(V);
+              {error, Reason} ->
+                Reason
             end,
-
-    build_results(Bucket, Rest, [{Key, Value}|Results]);
-build_results(_, [], Results) ->
+    multi_get(Bucket, Rest, [{Key, Value}|Results]);
+multi_get(_, [], Results) ->
     lists:flatten(mochijson:encode({struct, Results})).
 
 client() ->
@@ -78,3 +98,19 @@ get(C, Bucket, Key) ->
         Other ->
             Other
     end.
+
+multi_put(Bucket, [Object|Remaining], Results) ->
+  {struct, [{<<"key">>, Key}, {<<"data">>, Value}]} = Object,
+  EncodedValue = mochijson2:encode(Value),
+  Result = put(client(), list_to_binary(Bucket), Key, EncodedValue),
+  multi_put(Bucket, Remaining, [Result|Results]);
+multi_put(_Bucket, [], Results) ->
+  lists:reverse(Results).
+
+put(C, Bucket, Key, Value) ->
+  Obj = riak_object:new(Bucket, Key, Value),
+  case C:put(Obj) of
+    ok -> Key;
+    Other -> Other
+  end.
+
